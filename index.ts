f23024660ecece9988e7c107189810621f578fe4
Anais Betts <anais@anaisbetts.org>
2019-08-29 23:16:57 -0700

    Set up PgSQL + Hasura

--- index.ts
+++ index.ts
@@ -1,46 +1,60 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as docker from "@pulumi/docker";
+import * as pg from '@pulumi/postgresql';
 
-// This program encodes a complete application: a container running Redis Commander,
-// and a container running Redis. Redis Commander connects directly with Redis.
-//
-// In order for the two containers to communicate directly, they must be placed in the
-// same network. Before creating the containers themselves, we first create the network
-// that will contain both of them.
+const cfg = new pulumi.Config();
 const network = new docker.Network("net");
 
-// Also before creating a container, we must first obtain a "RemoteImage", which is a reference to an external image
-// that is downloaded to the local machine. In this case, we're referring to an image on Docker Hub.
-const redisImage = new docker.RemoteImage("redis-image", {
-  name: "redis:latest",
-  keepLocally: true // don't delete the image from the local machine when deleting this resource.
-});
-
-// We can create a container using a reference to the name of the image we just downloaded and a reference to the name
-// of the network that this container should use.
-const redisContainer = new docker.Container("redis", {
-  image: redisImage.name,
-  networksAdvanced: [{ name: network.name }],
-  restart: "on-failure"
-});
-
-// We do the same thing for the Redis Commander container.
-const redisCommanderImage = new docker.RemoteImage("redis-commander-image", {
-  name: "rediscommander/redis-commander:latest",
+const pgImg = new docker.RemoteImage("postgresql-image", {
+  name: "postgres:11",
   keepLocally: true
 });
 
-const redisCommanderContainer = new docker.Container("redis-commander", {
-  image: redisCommanderImage.name,
-  restart: "on-failure",
+const pgVol = new docker.Volume('pgdata');
+
+const pgContainer = new docker.Container("postgres", {
+  image: pgImg.name,
   networksAdvanced: [{ name: network.name }],
-  envs: [redisContainer.name.apply(name => `REDIS_HOST=${name}`)],
-  // Finally, we expose some ports. Redis Commander listens on port 8081, so we'll map that to external
-  // port 3000 for easy consumption on the user machine.
-  ports: [
-    {
-      internal: 8081,
-      external: 3000
-    }
-  ]
+  restart: "on-failure",
+  volumes: [{ volumeName: pgVol.name, containerPath: '/var/lib/postgresql/data' }],
+  envs: [
+    `POSTGRES_USER=${cfg.require('pguser')}`,
+    cfg.requireSecret('pgpass').apply(p => `POSTGRES_PASSWORD=${p}`),
+  ],
+  ports: [{ internal: 5432, external: 5432 }]
+});
+
+/* NB: This doesn't work! 
+pg.config.database = 'localhost';
+pg.config.port = 5432;
+pg.config.sslmode = 'disable';
+pg.config.sslMode = 'disable';
+pg.config.username = cfg.require('pguser');
+cfg.requireSecret('pgpass').apply(p => pg.config.password = p);
+
+const db = new pg.Database('ruhe');
+*/
+
+const hasuraImage = new docker.RemoteImage("hasura-image", {
+  name: "hasura/graphql-engine:v1.0.0-beta.6",
+  keepLocally: true
+});
+
+const hasuraContainer = new docker.Container("hasura", {
+  image: hasuraImage.name,
+  networksAdvanced: [{ name: network.name }],
+  restart: "on-failure",
+  envs: [
+    cfg.requireSecret('pgpass').apply(p => {
+      const u = cfg.require('pguser');
+
+      return pgContainer.name.apply(hn =>
+        `HASURA_GRAPHQL_DATABASE_URL=postgres://${u}:${p}@${hn}:5432/ruhe`);
+    }),
+    cfg.requireSecret('pgpass').apply(p =>
+      `HASURA_GRAPHQL_ADMIN_SECRET=${p}`),
+    `HASURA_GRAPHQL_JWT_SECRET={"type":"RS512", "jwk_url": "https://www.googleapis.com/service_accounts/v1/jwk/securetoken@system.gserviceaccount.com"}`,
+    `HASURA_GRAPHQL_UNAUTHORIZED_ROLE=anonymous`
+  ],
+  ports: [{ internal: 8080, external: 8080 }]
 });
