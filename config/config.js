d0dd6de8b2eef69819edc706d87b3c9294795c60
Anais Betts <anais@anaisbetts.org>
2019-10-24 15:56:07 -0700

    Add an example migration / Sequelize config

---
+++ config/config.js
@@ -0,0 +1,30 @@
+if (!process.env.PGSQL_DATABASE) {
+  throw new Error("Dot-source scripts/env.sh first!");
+}
+
+module.exports = {
+  "development": {
+    "username": process.env['PGSQL_USER'],
+    "password": process.env['PGSQL_PASS'],
+    "database": process.env['PGSQL_DATABASE'].replace(/^./, '').replace(/.$/, ''),
+    "host": "127.0.0.1",
+    "dialect": "postgresql",
+    "operatorsAliases": false
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "postgresql",
+    "operatorsAliases": false
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "postgresql",
+    "operatorsAliases": false
+  }
+}
