b4302d67f2948bd6c4acd51ea4a4511bc8fed4b9
Anais Betts <anais@anaisbetts.org>
2019-10-24 15:55:36 -0700

    Get the Pulumi script going

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ const cfg = new pulumi.Config();
 const network = new docker.Network('net');
 
 const pgImg = new docker.RemoteImage('postgresql-image', {
-  name: 'postgres:latest',
+  name: 'postgres:11',
   keepLocally: true
 });
 
@@ -15,7 +15,7 @@ const pgVol = new docker.Volume('pgdata');
 export const pgContainer = new docker.Container('postgres', {
   image: pgImg.name,
   networksAdvanced: [{ name: network.name }],
-  restart: 'on-failure',
+  restart: stallThenReturn(5 * 1000, 'on-failure'),
   volumes: [{ volumeName: pgVol.name, containerPath: '/var/lib/postgresql/data' }],
   envs: [
     `POSTGRES_USER=${cfg.require('pguser')}`,
@@ -34,10 +34,10 @@ const pgProvider = new pg.Provider('pg', {
   host: pgContainer.ipAddress,
   username: cfg.require('pguser'),
   password: cfg.requireSecret('pgpass'),
-  sslmode: stallThenReturn(5 * 1000, 'disable'),
+  sslmode: 'disable',
 });
 
-const db = new pg.Database('ruhe', {}, { provider: pgProvider });
+export const db = new pg.Database('ruhe', {}, { provider: pgProvider });
 
 const hasuraImage = new docker.RemoteImage('hasura-image', {
   name: 'hasura/graphql-engine:v1.0.0-beta.6',
@@ -52,13 +52,13 @@ export const hasuraContainer = new docker.Container('hasura', {
     cfg.requireSecret('pgpass').apply(p => {
       const u = cfg.require('pguser');
 
-      return pgContainer.name.apply(hn =>
-        `HASURA_GRAPHQL_DATABASE_URL=postgres://${u}:${p}@${hn}:5432/ruhe`);
+      return pgContainer.name.apply(hn => db.name.apply(name =>
+        `HASURA_GRAPHQL_DATABASE_URL=postgres://${u}:${p}@${hn}:5432/${name}`));
     }),
-    cfg.requireSecret('pgpass').apply(p =>
-      `HASURA_GRAPHQL_ADMIN_SECRET=${p}`),
-    `HASURA_GRAPHQL_JWT_SECRET={'type':'RS512', 'jwk_url': 'https://www.googleapis.com/service_accounts/v1/jwk/securetoken@system.gserviceaccount.com'}`,
+    cfg.requireSecret('pgpass').apply(p => `HASURA_GRAPHQL_ADMIN_SECRET=${p}`),
+    //`HASURA_GRAPHQL_JWT_SECRET="{'type':'RS512', 'jwk_url': 'https://www.googleapis.com/service_accounts/v1/jwk/securetoken@system.gserviceaccount.com'}"`,
+    `HASURA_GRAPHQL_ENABLE_CONSOLE=true`,
     `HASURA_GRAPHQL_UNAUTHORIZED_ROLE=anonymous`
   ],
-  ports: [{ internal: 8080, external: 8080 }]
+  ports: [{ internal: 8080, external: 8081 }]
 });
