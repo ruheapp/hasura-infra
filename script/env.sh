e0edad52d6792ca4fc9ecc05ac40d361d82ceb49
Anais Betts <anais@anaisbetts.org>
2019-10-24 18:04:17 -0700

    Export the Hasura URL as an env var

--- script/env.sh
+++ script/env.sh
@@ -3,3 +3,6 @@
 export PGSQL_DATABASE=`pulumi stack output -j | jq -r '.db.name'`
 export PGSQL_USER=`pulumi config get pguser`
 export PGSQL_PASS=`pulumi config get pgpass`
+
+PORT=`pulumi stack output -j | jq -r '.hasuraContainer.ports[0].external'`
+export HASURA_URL="http://localhost:$PORT"
